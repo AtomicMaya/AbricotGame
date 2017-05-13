e2963264ae21514eeaf4a4dcb8fa217977f063bc
fg1989 <fgazzett@gmail.com>
2017-05-13 20:40:51 +0200

    Update main.py

--- client final/main.py
+++ client final/main.py
@@ -8,6 +8,14 @@ from typing import Dict, Tuple
 from pathfinding import calculate_movement
 import time
 
+
+-def convert_image(chemin: str, couleurfond=(255, 255, 255)):		
+ -    """Cette image permet de transformer le chemin vers un fichier en image"""		
+ -    image = pygame.image.load(chemin)		
+ -    image.set_colorkey(couleurfond)		
+ -    return image.convert_alpha()
+
+
 class Map:
     """Cette classe représente une carte du jeu"""
 
