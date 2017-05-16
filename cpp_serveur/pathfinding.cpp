10f26faf81f185e21daa2ba681eac0e4ed3a056b
NB0174 <boeckh.nicolas@gmail.com>
2017-05-16 14:53:29 +0200

    php

---
+++ cpp_serveur/pathfinding.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class GridCell{
+    int x;
+    int y;
+    bool is_free_space;
+
+    GridCell(int cell_x, int cell_y, bool is_not_obstacle) {
+            x = cell_x
+            y = cell_y
+
+    }
+
+
+};
---
+++ cpp_serveur/pathfinding.h
@@ -0,0 +1,19 @@
+
+#ifndef DEF_PATHFINDER
+#define DEF_PATHFINDER
+
+class GridCell {
+    public:
+    void GridCell
+
+    private:
+    int x;
+    int y;
+    bool is_free_space;
+    GridCell parent;
+    int f;
+    int g;
+    int h;
+};
+
+#endif // DEF_PATHFINDER
