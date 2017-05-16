6c0cc0895a0a711f29f3026ea55511367a7775dd
leoTigers <jimenez.lorenzo@orange.fr>
2017-05-16 02:39:03 +0200

    php files

---
+++ gitphp/cretbdd.php
@@ -0,0 +1,32 @@
+<?php
+/*
+	class MyDB extends SQLite3
+	{
+		function __construct()
+		{
+			$this->open("login.db3");
+		}
+	}
+	
+	$db = new MyDB();
+	if(!$db){
+		echo "Erreur inconnue, veuillez réessayer plus tard.\n";
+	} 
+	
+	$sql =<<<EOF
+      CREATE TABLE LOGIN
+      (TOKEN           TEXT    NOT NULL,
+      TIME            INT     NOT NULL,
+	  LOGIN            TEXT   NOT NULL);
+EOF;
+
+	$ret = $db->exec($sql);
+	if(!$ret){
+		echo $db->lastErrorMsg();
+	} else {
+		echo "Records created successfully\n";
+	}	
+$db->close()
+
+*/
+?>
