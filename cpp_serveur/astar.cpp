7c729c49de6773f8ee5fad33bb8999c34d40a014
NB0174 <boeckh.nicolas@gmail.com>
2017-05-22 20:30:15 +0200

    a*

--- cpp_serveur/astar.cpp
+++ cpp_serveur/astar.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <queue>
-#include <string>
 #include <chrono>
 #include <list>
 #include <vector>
@@ -17,43 +16,43 @@ static int open_nodes_map[map_width][map_height]; // map of open (not-yet-tried)
 static int dir_map[map_width][map_height]; // map of directions
 
 const enum Movement { RIGHT = 0, DOWN = 1, LEFT = 2, UP = 3 };
-static int dx[4]={1, 0, -1, 0};
-static int dy[4]={0, 1, 0, -1};
+static int dx[4] = { 1, 0, -1, 0 };
+static int dy[4] = { 0, 1, 0, -1 };
 
 // A-star algorithm.
 // The route returned is a string of direction digits.
 
-vector<Movement> pathFind(int xStart, int yStart,	int xFinish, int yFinish) {
-	static priority_queue<GridCell> pq[2]; // list of open (not-yet-tried) nodes
-	static int pqi; // pq index
+vector<Movement> pathFind(int xStart, int yStart, int xFinish, int yFinish) {
+	static priority_queue<GridCell> p_queue[2]; // Open list
+	static int p_queue_index;
 	static GridCell* n0;
 	static GridCell* m0;
 	static int c, i, j, x, y, xdx, ydy;
-	pqi = 0;
+	p_queue_index = 0;
 
-	// reset the node maps
+	// Reset Nodes
 	for (y = 0; y < map_height; y++) {
-		for (x = 0; x<map_width; x++) {
+		for (x = 0; x < map_width; x++) {
 			closed_nodes_map[x][y] = 0;
 			open_nodes_map[x][y] = 0;
 		}
 	}
 
-	// create the start node and push into list of open nodes
+	// Create Start node
 	n0 = new GridCell(xStart, yStart, 0, 0);
 	n0->update_priority(xFinish, yFinish);
-	pq[pqi].push(*n0);
+	p_queue[p_queue_index].push(*n0);
 	open_nodes_map[x][y] = n0->get_priority(); // mark it on the open nodes map
 
 
-	while (!pq[pqi].empty())
+	while (!p_queue[p_queue_index].empty())
 	{
-		n0 = new GridCell(pq[pqi].top().get_x(), pq[pqi].top().get_y(),
-			pq[pqi].top().get_level(), pq[pqi].top().get_priority());
+		n0 = new GridCell(p_queue[p_queue_index].top().get_x(), p_queue[p_queue_index].top().get_y(),
+			p_queue[p_queue_index].top().get_level(), p_queue[p_queue_index].top().get_priority());
 
 		x = n0->get_x(); y = n0->get_y();
 
-		pq[pqi].pop();
+		p_queue[p_queue_index].pop();
 		open_nodes_map[x][y] = 0;
 		closed_nodes_map[x][y] = 1;
 		if (x == xFinish && y == yFinish) {
@@ -68,7 +67,7 @@ vector<Movement> pathFind(int xStart, int yStart,	int xFinish, int yFinish) {
 			}
 
 			delete n0;
-			while (!pq[pqi].empty()) pq[pqi].pop();
+			while (!p_queue[p_queue_index].empty()) p_queue[p_queue_index].pop();
 			return path;
 		}
 
@@ -76,7 +75,7 @@ vector<Movement> pathFind(int xStart, int yStart,	int xFinish, int yFinish) {
 		{
 			xdx = x + dx[i]; ydy = y + dy[i];
 
-			if (!(xdx<0 || xdx>map_width - 1 || ydy<0 || ydy>map_height - 1 || map[xdx][ydy] == 1
+			if (!(xdx < 0 || xdx > map_width - 1 || ydy < 0 || ydy > map_height - 1 || map[xdx][ydy] == 1
 				|| closed_nodes_map[xdx][ydy] == 1))
 			{
 				m0 = new GridCell(xdx, ydy, n0->get_level(),
@@ -87,31 +86,31 @@ vector<Movement> pathFind(int xStart, int yStart,	int xFinish, int yFinish) {
 				if (open_nodes_map[xdx][ydy] == 0)
 				{
 					open_nodes_map[xdx][ydy] = m0->get_priority();
-					pq[pqi].push(*m0);
+					p_queue[p_queue_index].push(*m0);
 					dir_map[xdx][ydy] = (i + 2) % 4;
 				}
-				else if (open_nodes_map[xdx][ydy]>m0->get_priority())
+				else if (open_nodes_map[xdx][ydy] > m0->get_priority())
 				{
 					open_nodes_map[xdx][ydy] = m0->get_priority();
 					dir_map[xdx][ydy] = (i + 2) % 4;
 
-					while (!(pq[pqi].top().get_x() == xdx &&
-						pq[pqi].top().get_y() == ydy))
+					while (!(p_queue[p_queue_index].top().get_x() == xdx &&
+						p_queue[p_queue_index].top().get_y() == ydy))
 					{
-						pq[1 - pqi].push(pq[pqi].top());
-						pq[pqi].pop();
+						p_queue[1 - p_queue_index].push(p_queue[p_queue_index].top());
+						p_queue[p_queue_index].pop();
 					}
-					pq[pqi].pop();
-					if (pq[pqi].size()>pq[1 - pqi].size()) pqi = 1 - pqi;
-					while (!pq[pqi].empty())
+					p_queue[p_queue_index].pop();
+					if (p_queue[p_queue_index].size()>p_queue[1 - p_queue_index].size()) p_queue_index = 1 - p_queue_index;
+					while (!p_queue[p_queue_index].empty())
 					{
-						pq[1 - pqi].push(pq[pqi].top());
-						pq[pqi].pop();
+						p_queue[1 - p_queue_index].push(p_queue[p_queue_index].top());
+						p_queue[p_queue_index].pop();
 					}
-					pqi = 1 - pqi;
-					pq[pqi].push(*m0);
+					p_queue_index = 1 - p_queue_index;
+					p_queue[p_queue_index].push(*m0);
 				}
-				else delete m0; 
+				else delete m0;
 			}
 		}
 		delete n0;
@@ -126,14 +125,23 @@ vector<Movement> calculate_movement(Coordinates start, Coordinates end) {
 	high_resolution_clock::time_point t1 = high_resolution_clock::now();
 	vector<Movement> route = pathFind(x_start, y_start, x_end, y_end);
 	high_resolution_clock::time_point t2 = high_resolution_clock::now();
-	auto duration1 = duration_cast<nanoseconds>(t2 - t1).count();
-	cout << "Time to calculate the route (ns): " << duration1 << endl;
+	auto duration1 = duration_cast<milliseconds>(t2 - t1).count();
+	cout << "Time to calculate the route (ms): " << duration1 << endl;
 	return route;
 }
 
 int main() {
 	Coordinates start = Coordinates(0, 0), end = Coordinates(31, 17);
-	vector<Movement> path = calculate_movement(start, end);
+	int x_start = start.m_x, y_start = start.m_y, x_end = end.m_x, y_end = end.m_y;
+
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+	vector<Movement> route = pathFind(x_start, y_start, x_end, y_end);
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	auto duration1 = duration_cast<microseconds>(t2 - t1).count();
+	cout << "Time to calculate the route (mus): " << duration1 << endl;
+	//for (int i : route) {
+	//cout << i;
+	//}
 	getchar();
 	return 0;
 }
